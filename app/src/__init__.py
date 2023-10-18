a5ef9f304f909df436235ea525c87ed20b6dc90c
Elisa Rachel <elisa.martins@fatec.sp.gov.br>
2023-10-18 18:08:07 -0300

    Arrumei pequenos erros nos arquivos python

--- app/src/__init__.py
+++ app/src/__init__.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, render_template
 from flaskext.mysql import MySQL
 
@@ -6,12 +5,13 @@ mysql = MySQL()
 
 def create_app():
 	app = Flask("__name__", instance_relative_config=False)
-    app.config.from_object('config.Config')
+	app.config.from_object('config.Config')
 
 	mysql.init_app(app)
 
 	with app.app_context():
 		from . import routes
+		from . import auth
 
 		app.register_blueprint(auth.auth_bp)
 
