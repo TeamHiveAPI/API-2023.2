4bd0e82a1d421b9742b2c41c3a25475685f52e2e
Joyce Aparecida <joycepereirasbs@gmail.com>
2023-10-18 17:40:54 -0300

    arrumei todas as rotas para as pagina e adicionei o arquivo base

--- app/src/routes.py
+++ app/src/routes.py
@@ -1,18 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
-app = Flask(
-    __name__,
-    template_folder="templates"
-)
+app = Flask(__name__)
 
+
+#Rota pagina inicial
 @app.route('/')
-def home():
-    return render_template('index.html', title='Home')
+def index():
+    return render_template('index.html',nav= 'active' )
 
+#Rota pagina blog 
 @app.route('/blog')
 def blog():
-    return render_template('blog.html', title="Blog")
+    return render_template('blog.html', nav= 'active')
 
+#Rota pagina dados 
 @app.route('/dados')
 def dados():
-    return render_template('dados.html', title="Dados")
+    return render_template('dados.html', nav= 'active')
+
+#Rota pagina login
+@app.route('/login')
+def login():
+    return render_template('login.html', nav= 'active', titulo = 'LOGIN')
+
+#Rota pagina cadastro
+@app.route ('/cadastro')
+def cadastro():
+    return render_template('cadastro.html', nav= 'active')
+
+#Rota minha conta 
+@app.route('/minhaconta')
+def conta():
+    return render_template('minhaconta.html', nav= 'active', titulo = 'MINHA-CONTA')
+
+app.run(debug=True)
+
