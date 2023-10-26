fc1ec80b8f9f01b9cb610565594bcf9360ccd274
Joyce Silva <142221695+joycesilvaaa@users.noreply.github.com>
2023-10-26 20:36:04 -0300

    reconfigurei o arquivo controller

--- app/controller.py
+++ app/controller.py
@@ -1,7 +1,17 @@
-from src import create_app
+#aplicação principal
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
-app = create_app()
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+db = SQLAlchemy(app)
+
+
+from  routes import  *
+
+# Para fazer rodar a aplicação sem precisar ficar reinicializar toda vez
+if __name__ == '__main__':
+    app.run(debug=True)
