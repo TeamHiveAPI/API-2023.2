ec22088a35926aebb7ae772bc11755023ad0a1f5
Marco Antonio Arantes <marcoantoniopou@gmail.com>
2023-10-28 17:10:57 -0300

    Estilizei a área de fazer um post no Blog e adicionei um botão funcional de adicionar imagens através de um modal

--- app/src/static/script.js
+++ app/src/static/script.js
@@ -23,3 +23,53 @@ let calcScrollValue = () => {
 window.onscroll = calcScrollValue;
 window.onload = calcScrollValue;
 
+
+// Modal Selecionar Imagem //
+
+
+// Get the modal
+var card_modal = document.getElementById("modal");
+
+// Get the button that opens the modal
+var botao_modal_1 = document.getElementById("botao_modal_1");
+
+// Get the <span> element that closes the modal
+var voltar = document.getElementsByClassName("modal_baixo_botao_voltar")[0];
+
+var sim = document.getElementsByClassName("modal_baixo_botao_sim")[0];
+
+// When the user clicks the button, open the modal 
+botao_modal_1.onclick = function() {
+    card_modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+voltar.onclick = function() {
+    card_modal.style.display = "none";
+};
+
+sim.onclick = function() {
+    card_modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    card_modal.style.display = "none";
+  };
+};
+
+// Validar se usuário inseriu uma imagem
+
+function validateForm() {
+    var x = document.forms["selecionar_imagem"]["imagem"].value;
+    if (x == "" || x == null) {
+        alert("Você precisa selecionar pelo menos 1 imagem!");
+        return false;
+    }
+    
+    if (x > 5) {
+        alert("Selecione no máximo 5 imagens!");
+        return false;
+    }
+}
