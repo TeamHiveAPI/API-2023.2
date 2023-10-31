66eed178ee723fb4ade6b5a43f502137a1d86811
Joyce Silva <142221695+joycesilvaaa@users.noreply.github.com>
2023-10-31 10:58:50 -0300

    editei class usuario

--- app/src/models.py
+++ app/src/models.py
@@ -1,29 +1,32 @@
 from controller import db
+from sqlalchemy import BINARY
 
-#conexão com as tabelas
+# Conexão com as tabelas
 class Usuario(db.Model):
-	__tablename__ = "usuario"
-	id = db.Column(db.Integer,primary_key=True, autoincrement=True)
-	nome = db.Column(db.String(64),nullable=False)
-	dn = db.Column(db.Date,nullable=False)
-	cpf = db.Column(db.String(11),unique=True,nullable=False)
-	endereco = db.Column(db.String(100),nullable=False)
-	email = db.Column(db.String(80),unique=True,nullable=False)
-	parentesco = db.Column(db.String(10),nullable=False)
-	senha = db.Column(db.String(200),nullable=False)
-	profissao = db.Column(db.String(20),nullable=False)
-	comochegou = db.Column(db.String(22),nullable=False)
+    __tablename__ = "usuario"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(64), nullable=False)
+    dn = db.Column(db.Date, nullable=False)
+    cpf = db.Column(db.String(11), unique=True, nullable=False)
+    endereco = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    parentesco = db.Column(db.String(10), nullable=False)
+    senha = db.Column(db.String(200), nullable=False)
+    profissao = db.Column(db.String(20), nullable=False)
+    comochegou = db.Column(db.String(22), nullable=False)
+    foto_perfil = db.Column(BINARY)
 
-	def __init__(self, nome, dn, cpf, endereco, email, parentesco, senha, profissao, comochegou):
-		self.nome = nome 
-		self.dn = dn
-		self.cpf = cpf
-		self.endereco = endereco
-		self.email = email
-		self.parentesco = parentesco
-		self.senha = senha 
-		self.profissao = profissao
-		self.comochegou = comochegou
+    def __init__(self, nome, dn, cpf, endereco, email, parentesco, senha, profissao, comochegou, foto_perfil):
+        self.nome = nome
+        self.dn = dn
+        self.cpf = cpf
+        self.endereco = endereco
+        self.email = email
+        self.parentesco = parentesco
+        self.senha = senha
+        self.profissao = profissao
+        self.comochegou = comochegou
+        self.foto_perfil = foto_perfil
 
 # Criação de uma tabela para as imagens, associada aos posts
 class Imagem(db.Model):
