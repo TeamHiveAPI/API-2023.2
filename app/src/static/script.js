5e9305ef158be0d191dab28db72fc9ef77b8e1b5
Júlia Rosado <juliamariahhr@gmail.com>
2023-11-03 19:15:39 -0300

    arrumei o script do modal

--- app/src/static/script.js
+++ app/src/static/script.js
@@ -1,22 +1,24 @@
-let calcScrollValue = () => {
+var calcScrollValue = () => {
     let scrollProgress = document.getElementById("progress");
     let progressValue = document.getElementById("progress_value");
     let pos = document.documentElement.scrollTop;
     let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     let scrollValue = Math.round((pos * 100) / calcHeight);
 
-    if (pos > 100) {
-        scrollProgress.style.display = "flex";
+    if (scrollProgress){
+        if (pos > 100) {
+            scrollProgress.style.display = "flex";
+        }
+        else {
+            scrollProgress.style.display = "none";
+        }
+    
+        scrollProgress.addEventListener("click", () => {
+            document.documentElement.scrollTop = 0;
+        });
+    
+        scrollProgress.style.background = `conic-gradient(#0F9635 ${scrollValue}%, #d7d7d7 ${scrollValue}%`
     }
-    else {
-        scrollProgress.style.display = "none";
-    }
-
-    scrollProgress.addEventListener("click", () => {
-        document.documentElement.scrollTop = 0;
-    });
-
-    scrollProgress.style.background = `conic-gradient(#0F9635 ${scrollValue}%, #d7d7d7 ${scrollValue}%`
 };
 
 
@@ -25,37 +27,38 @@ window.onload = calcScrollValue;
 
 // Modal Selecionar Imagem //
 
+window.onload = function() {
+    // Get the modal
+    var card_modal = document.getElementById("modal");
 
-// Get the modal
-var card_modal = document.getElementById("modal");
+    // Get the button that opens the modal
+    var botao_modal_1 = document.getElementById("botao_modal_1");
 
-// Get the button that opens the modal
-var botao_modal_1 = document.getElementById("botao_modal_1");
+    // Get the <span> element that closes the modal
+    var voltar = document.getElementsByClassName("modal_baixo_botao_voltar")[0];
 
-// Get the <span> element that closes the modal
-var voltar = document.getElementsByClassName("modal_baixo_botao_voltar")[0];
+    var sim = document.getElementsByClassName("modal_baixo_botao_sim")[0];
 
-var sim = document.getElementsByClassName("modal_baixo_botao_sim")[0];
+    // When the user clicks the button, open the modal 
+    botao_modal_1.onclick = function() {
+        card_modal.style.display = "block";
+    };
 
-// When the user clicks the button, open the modal 
-botao_modal_1.onclick = function() {
-    card_modal.style.display = "block";
-};
+    // When the user clicks on <span> (x), close the modal
+    voltar.onclick = function() {
+        card_modal.style.display = "none";
+    };
 
-// When the user clicks on <span> (x), close the modal
-voltar.onclick = function() {
-    card_modal.style.display = "none";
-};
+    sim.onclick = function() {
+        card_modal.style.display = "none";
+    };
 
-sim.onclick = function() {
-    card_modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    card_modal.style.display = "none";
-  };
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        card_modal.style.display = "none";
+      };
+    };
 };
 
 // Validar se usuário inseriu uma imagem
