2dc5735b823fae0da2e7bcbc3f4cfa084bd98264
Joyce Silva <142221695+joycesilvaaa@users.noreply.github.com>
2023-11-18 23:51:53 -0300

    adicionei o codigo para aparecer os flash

--- app/src/controller.py
+++ app/src/controller.py
@@ -12,8 +12,17 @@ db = SQLAlchemy(app)
 
 from routes import *
 
-# Para fazer rodar a aplicação sem precisar ficar reinicializar toda vez
-if __name__ == '__main__':
+def inicializando_app():
     with app.app_context():
         db.create_all()
+        # Verifica se o usuário administrador já existe antes de adicioná-lo
+        admin_user = Usuario.query.filter_by(email='admrimdoamor2023@timehive.com').first()
+        if admin_user is None:
+            adm = Usuario(nome='Adm-Rim do Amor', dn='2023-09-01', cpf='00000000011', endereco='@rimdoamor', email='admrimdoamor2023@timehive.com', parentesco='outro', senha='rimdoamor2023', profissao='Administrador', comochegou='outro', is_admin=1)
+            db.session.add(adm)
+            db.session.commit()
+
+# Para fazer rodar a aplicação sem precisar ficar reinicializar toda vez
+if __name__ == '__main__':
+    inicializando_app()
     app.run(debug=True)
